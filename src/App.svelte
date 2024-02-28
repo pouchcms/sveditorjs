9f00173eed00f1c32c0676e583ed8da2f09068f4
pouchcms <pouchcms@gmail.com>
2024-02-29 00:35:35 +0300

    changed import

--- src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  //this for testing purpose
-  import {Editor,editor} from "sveditorjs"; 
- 
-
-</script>
-<Editor>
-</Editor>
-
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import App from './App.svelte';
-
-var app = new App({
-	target: document.querySelector('.sveditor-cont')
-});
-
-export default app;
